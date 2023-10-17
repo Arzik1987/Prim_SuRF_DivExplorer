545d0795d8accc97325ef826ea159b1c4fe9b08a
Vadim Arzamasov <arzamasov@i40pool.ipd.kit.edu>
2023-10-17 11:03:33 +0200

    modified files

---
+++ acc_experiments.R
@@ -0,0 +1,226 @@
+library(prim)
+source("prim_edits.R")
+setwd("C:\\Users\\arzamasov\\Desktop\\SuRF-Reproducibility")
+
+save_res_to_file <- function(boxes, filename) {
+  for(i in 1:length(boxes)){
+    boxes[[i]] <- c(boxes[[i]][1,], boxes[[i]][2,]-boxes[[i]][1,])
+  }
+  mat <- do.call(rbind, boxes)
+  write.table(mat, filename, row.names = FALSE, col.names = FALSE, sep = ",")
+}
+
+
+for(k in 1:5){
+  print(k)
+  d <- read.table(paste0("input\\data_aggr_dimensions=", k, "_multi_False"), sep=',')
+  inds <- which(apply(d[, 1:k, drop=FALSE], 1, function(row) all(0.6 < row & row < 0.9)))
+  
+  box <- matrix(c(rep(0, k),rep(1, k)), ncol = k, byrow = TRUE)
+  gt <- list(matrix(c(rep(0.6, k),rep(0.9, k)), ncol = k, byrow = TRUE))
+  mass.min = 0.3^k
+  thr = 2
+  
+  res <- tryCatch({
+    prim.box(d[, 1:k, drop = FALSE], d[,k + 1], box.init = box, threshold = thr, 
+             threshold.type = 1, mass.min = mass.min, 
+             peel.alpha = 0.05)
+  }, error=function(e) {
+    NA
+  })
+  
+  if(!is.list(res)){
+    save_res_to_file(list(box), paste0("prim_boxes\\data_aggr_dimensions=", k, "_multi_False.csv"))
+  } else {
+    save_res_to_file(res$box, paste0("prim_boxes\\data_aggr_dimensions=", k, "_multi_False.csv"))
+  } 
+}
+
+
+for(k in 1:5){
+  print(k)
+  d <- read.table(paste0("input\\data_aggr_dimensions=", k, "_multi_True"), sep=',')
+  
+  box <- matrix(c(rep(0, k),rep(1, k)), ncol = k, byrow = TRUE)
+  gt <- list(matrix(c(rep(0, k),rep(0.2, k)), ncol = k, byrow = TRUE),
+             matrix(c(rep(0.3, k),rep(0.5, k)), ncol = k, byrow = TRUE),
+             matrix(c(rep(0.6, k),rep(0.8, k)), ncol = k, byrow = TRUE))
+  mass.min = 0.2^k
+  thr = 2
+  
+  res <- tryCatch({
+    prim.box(d[, 1:k, drop = FALSE], d[,k + 1], box.init = box, threshold = thr, 
+             threshold.type = 1, mass.min = mass.min, 
+             peel.alpha = 0.05)
+  }, error=function(e) {
+    NA
+  })
+  
+  if(!is.list(res)){
+    save_res_to_file(list(box), paste0("prim_boxes\\data_aggr_dimensions=", k, "_multi_True.csv"))
+  } else {
+    save_res_to_file(res$box, paste0("prim_boxes\\data_aggr_dimensions=", k, "_multi_True.csv"))
+  } 
+}
+
+
+library(FNN)
+
+for(k in 1:5){
+  print(k)
+  d <- read.table(paste0("input\\data_density_dimensions=", k, "_multi_False"), sep=',')
+  
+  d.tmp <- as.data.frame(matrix(runif(100000*k), ncol = k))
+  d.tmp[, k + 1] <- -get.knnx(d, d.tmp, k = 1)$nn.dist[, 1]
+  d <- d.tmp
+  
+  inds <- which(apply(d[, 1:k, drop=FALSE], 1, function(row) all(0.6 < row & row < 0.9)))
+  
+  box <- matrix(c(rep(0, k),rep(1, k)), ncol = k, byrow = TRUE)
+  gt <- list(matrix(c(rep(0.6, k),rep(0.9, k)), ncol = k, byrow = TRUE))
+  mass.min = 0.3^k
+  thr = 1.1*mean(d[inds, k + 1])
+  
+  res <- tryCatch({
+    prim.box(d[, 1:k, drop = FALSE], d[,k + 1], box.init = box, threshold = thr, 
+             threshold.type = 1, mass.min = mass.min, 
+             peel.alpha = 0.05)
+  }, error=function(e) {
+    NA
+  })
+  
+  if(!is.list(res)){
+    save_res_to_file(list(box), paste0("prim_boxes\\data_density_dimensions=", k, "_multi_False.csv"))
+  } else {
+    save_res_to_file(res$box, paste0("prim_boxes\\data_density_dimensions=", k, "_multi_False.csv"))
+  }
+}
+
+for(k in 1:5){
+  print(k)
+  d <- read.table(paste0("input\\data_density_dimensions=", k, "_multi_True"), sep=',')
+  
+  d.tmp <- as.data.frame(matrix(runif(100000*k), ncol = k))
+  d.tmp[, k + 1] <- -get.knnx(d, d.tmp, k = 1)$nn.dist[, 1]
+  d <- d.tmp
+  
+  avgs <- numeric()
+  for(i in 0:2){
+    inds <- which(apply(d[, 1:k, drop=FALSE], 1, function(row) all(0.3*i < row & row < 0.3*i+0.2)))
+    avgs <- c(avgs, mean(d[inds, k + 1]))
+  }
+  
+  box <- matrix(c(rep(0, k),rep(1, k)), ncol = k, byrow = TRUE)
+  gt <- list(matrix(c(rep(0, k),rep(0.2, k)), ncol = k, byrow = TRUE),
+             matrix(c(rep(0.3, k),rep(0.5, k)), ncol = k, byrow = TRUE),
+             matrix(c(rep(0.6, k),rep(0.8, k)), ncol = k, byrow = TRUE))
+  mass.min = 0.2^k
+  thr = 1.5*min(avgs)
+  
+  res <- tryCatch({
+    prim.box(d[, 1:k, drop = FALSE], d[,k + 1], box.init = box, threshold = thr, 
+             threshold.type = 1, mass.min = mass.min, 
+             peel.alpha = 0.05)
+  }, error=function(e) {
+    NA
+  })
+  
+  if(!is.list(res)){
+    save_res_to_file(list(box), paste0("prim_boxes\\data_density_dimensions=", k, "_multi_True.csv"))
+  } else {
+    save_res_to_file(res$box, paste0("prim_boxes\\data_density_dimensions=", k, "_multi_True.csv"))
+  }
+}
+
+#### KDE
+
+
+for(k in 1:4){
+  print(k)
+  d <- read.table(paste0("input\\data_density_dimensions=", k, "_multi_False"), sep=',')
+
+  if (k == 1){
+    kde_result = density(d[,1])
+    d <- as.data.frame(matrix(runif(100000*k), ncol = k))
+    d[,2] = approx(x = kde_result$x, y = kde_result$y, xout = d[,1])$y
+  } else {
+    library(ks)
+    kde_result <- kde(d)
+    d <- as.data.frame(matrix(runif(100000*k), ncol = k))
+    d[, k + 1] <- predict(kde_result, x = d)
+  }
+
+  inds <- which(apply(d[, 1:k, drop=FALSE], 1, function(row) all(0.6 < row & row < 0.9)))
+
+  box <- matrix(c(rep(0, k),rep(1, k)), ncol = k, byrow = TRUE)
+  gt <- list(matrix(c(rep(0.6, k),rep(0.9, k)), ncol = k, byrow = TRUE))
+  mass.min = 0.3^k
+  thr = 0.9*mean(d[inds, k + 1])
+  
+  res <- tryCatch({
+    prim.box(d[, 1:k, drop = FALSE], d[,k + 1], box.init = box, threshold = thr, 
+             threshold.type = 1, mass.min = mass.min, 
+             peel.alpha = 0.05)
+  }, error=function(e) {
+    NA
+  })
+  
+  if(!is.list(res)){
+    save_res_to_file(list(box), paste0("prim_kde\\data_density_dimensions=", k, "_multi_False.csv"))
+  } else {
+    save_res_to_file(res$box, paste0("prim_kde\\data_density_dimensions=", k, "_multi_False.csv"))
+  }
+}
+
+box <- matrix(c(rep(0, 5),rep(1, 5)), ncol = 5, byrow = TRUE)
+save_res_to_file(list(box), paste0("prim_kde\\data_density_dimensions=", 5, "_multi_False.csv"))
+
+
+for(k in 1:4){
+  print(k)
+  d <- read.table(paste0("input\\data_density_dimensions=", k, "_multi_True"), sep=',')
+  
+  if (k == 1){
+    kde_result = density(d[,1])
+    d <- as.data.frame(matrix(runif(100000*k), ncol = k))
+    d[,2] = approx(x = kde_result$x, y = kde_result$y, xout = d[,1])$y
+  } else {
+    library(ks)
+    kde_result <- kde(d)
+    d <- as.data.frame(matrix(runif(100000*k), ncol = k))
+    d[, k + 1] <- predict(kde_result, x = d)
+  }
+  
+  avgs <- numeric()
+  for(i in 0:2){
+    inds <- which(apply(d[, 1:k, drop=FALSE], 1, function(row) all(0.3*i < row & row < 0.3*i+0.2)))
+    avgs <- c(avgs, mean(d[inds, k + 1]))
+  }
+  
+  box <- matrix(c(rep(0, k),rep(1, k)), ncol = k, byrow = TRUE)
+  gt <- list(matrix(c(rep(0, k),rep(0.2, k)), ncol = k, byrow = TRUE),
+             matrix(c(rep(0.3, k),rep(0.5, k)), ncol = k, byrow = TRUE),
+             matrix(c(rep(0.6, k),rep(0.8, k)), ncol = k, byrow = TRUE))
+  mass.min = 0.2^k
+  thr = 0.66*min(avgs)
+  
+  res <- tryCatch({
+    prim.box(d[, 1:k, drop = FALSE], d[,k + 1], box.init = box, threshold = thr, 
+             threshold.type = 1, mass.min = mass.min, 
+             peel.alpha = 0.05)
+  }, error=function(e) {
+    NA
+  })
+  
+  if(!is.list(res)){
+    save_res_to_file(list(box), paste0("prim_kde\\data_density_dimensions=", k, "_multi_True.csv"))
+  } else {
+    save_res_to_file(res$box, paste0("prim_kde\\data_density_dimensions=", k, "_multi_True.csv"))
+  }
+}
+
+box <- matrix(c(rep(0, 5),rep(1, 5)), ncol = 5, byrow = TRUE)
+save_res_to_file(list(box), paste0("prim_kde\\data_density_dimensions=", 5, "_multi_True.csv"))
+
+
+
--- prim_dens.py
+++ prim_dens.py
@@ -1,22 +1,25 @@
 import numpy as np
 
 class PRIMdens:
-    def __init__(self, alpha=0.05):
+    def __init__(self, X, alpha=0.05):
         self.alpha = alpha
         self.boxes_ = []  # To store intermediate boxes
         self.densities_ = []  # To store densities of intermediate boxes
+        self.X_ = X  	
 
-    def fit(self, X):
+    def fit(self):
         # Initial box is the unit box
-        box = np.vstack((np.zeros(X.shape[1]), np.ones(X.shape[1])))
-        n_points = X.shape[0]
+        box = np.vstack((np.zeros(self.X_.shape[1]), np.ones(self.X_.shape[1])))
+        n_points = self.X_.shape[0]
         
-        for iteration in range(100):
+        for iteration in range(120):
             max_density = -np.inf
             best_cut = None
             best_box = None
+            n_in_best_box = n_points  # Initially, all points are inside the box
+            ind_in_best_box = None # Initially, all points are inside the box
 
-            for dim in range(X.shape[1]):
+            for dim in range(self.X_.shape[1]):
                 for direction in [0, 1]:  # 0 for lower bound, 1 for upper bound
                     trial_box = box.copy()
                     current_dim_length = trial_box[1, dim] - trial_box[0, dim]
@@ -25,12 +28,13 @@ class PRIMdens:
                     adjustment = current_dim_length * self.alpha
                     if direction == 0:
                         trial_box[0, dim] += adjustment
+                        in_box = self.X_[:,dim] >= trial_box[0, dim]
                     else:
                         trial_box[1, dim] -= adjustment
+                        in_box = self.X_[:,dim] <= trial_box[1, dim]
                     
                     # Count points in the trial box
-                    in_box = np.all((X >= trial_box[0]) & (X <= trial_box[1]), axis=1)
-                    n_in_box = np.sum(in_box)
+                    n_in_box = np.count_nonzero(in_box)
                     
                     # Calculate volume of the trial box
                     volume = np.prod(trial_box[1] - trial_box[0])
@@ -42,15 +46,18 @@ class PRIMdens:
                         max_density = density
                         best_cut = (dim, direction)
                         best_box = trial_box
+                        n_in_best_box = n_in_box  # Update the count for best box
+                        ind_in_best_box = in_box
 
             # If we can't find a cut that improves density or the box contains less than 1/alpha points, break
-            if best_cut is None or np.sum(np.all((X >= best_box[0]) & (X <= best_box[1]), axis=1)) < 1/self.alpha:
+            if best_cut is None or n_in_best_box < 1/self.alpha:
                 break
 
             # Update the box and record it
             box = best_box
             self.boxes_.append(box)
             self.densities_.append(max_density)
+            self.X_ = self.X_[ind_in_best_box]
 
         return self
 
